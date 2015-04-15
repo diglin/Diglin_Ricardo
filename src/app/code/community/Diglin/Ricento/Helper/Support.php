fb1c0ef2d38d0197ea169e1032114ea829cc150d
Sylvain Rayé <sly@diglin.com>
2015-04-15 02:22:12 +0200

    Add a capability to send support archive per email directly to diglin

--- src/app/code/community/Diglin/Ricento/Helper/Support.php
+++ src/app/code/community/Diglin/Ricento/Helper/Support.php
@@ -15,6 +15,214 @@
 class Diglin_Ricento_Helper_Support extends Mage_Core_Helper_Abstract
 {
     /**
+     * @return string
+     */
+    public function exportAll()
+    {
+        $folder = Mage::getBaseDir('tmp') . DS . 'export' . DS . 'ricento';
+        $io = new Varien_Io_File();
+        $io->mkdir($folder);
+
+        $this->getPhpInfo($folder);
+        $this->getPhpExtensions($folder);
+        $this->getRicentoConfig($folder);
+        $this->getDeveloperConfig($folder);
+        $this->getRicentoTables($folder);
+        $this->getMagentoModules($folder);
+        $this->getMagentoConfig($folder);
+
+        $exceptionFile = Mage::getStoreConfig(Mage_Core_Model_Session_Abstract::XML_PATH_LOG_EXCEPTION_FILE);
+//        $systemFile = Mage::getStoreConfig('dev/log/file');
+
+        $io->cp(Mage::getBaseDir('var') . DS . 'log' . DS . Diglin_Ricento_Helper_Data::LOG_FILE, $folder . DS . Diglin_Ricento_Helper_Data::LOG_FILE);
+        $io->cp(Mage::getBaseDir('var') . DS . 'log' . DS . $exceptionFile, $folder . DS . $exceptionFile);
+
+        $destination = $folder . 'tar';
+        $tar = new Mage_Archive_Tar();
+        $tar->pack($folder, $destination);
+
+        $gzDestination = $destination . '.gz';
+        $tar = new Mage_Archive_Gz();
+        $tar->pack($destination, $gzDestination);
+
+        $io->rmdir($folder, true);
+
+        return $gzDestination;
+    }
+
+    /**
+     * @param $destination
+     * @return mixed
+     */
+    public function getPhpInfo($destination)
+    {
+        $phpinfo = new Varien_Object(Mage::helper('diglin_ricento/support')->getPhpInfoArray());
+        return $this->writeFile($destination, 'phpinfo.txt', $phpinfo->toJson());
+    }
+
+    /**
+     * @param $destination
+     * @return mixed
+     */
+    public function getRicentoConfig($destination)
+    {
+        $xml = Mage::getConfig()->getNode('default/ricento')->asNiceXml();
+        return $this->writeFile($destination, 'ricento_config.xml', $xml);
+    }
+
+    /**
+     * @param $destination
+     * @return mixed
+     */
+    public function getDeveloperConfig($destination)
+    {
+        $xml = Mage::getConfig()->getNode('default/dev')->asNiceXml();
+        return $this->writeFile($destination, 'config_dev.xml', $xml);
+    }
+
+    /**
+     * @param $destination
+     * @return array
+     */
+    public function getRicentoTables($destination)
+    {
+        $resource = Mage::getSingleton('core/resource');
+        $read = $resource->getConnection('core_read');
+
+        $tables = array(
+            'entity_id_001'         => $resource->getTableName('diglin_ricento/api_token'),
+            'entity_id_002'         => $resource->getTableName('diglin_ricento/products_listing'),
+            'item_id_003'           => $resource->getTableName('diglin_ricento/products_listing_item'),
+            'log_id_004'            => $resource->getTableName('diglin_ricento/listing_log'),
+            'entity_id_005'         => $resource->getTableName('diglin_ricento/sales_options'),
+            'transaction_id_006'    => $resource->getTableName('diglin_ricento/sales_transaction'),
+            'rule_id_007'           => $resource->getTableName('diglin_ricento/shipping_payment_rule'),
+            'job_id_008'            => $resource->getTableName('diglin_ricento/sync_job'),
+            'job_listing_id_009'    => $resource->getTableName('diglin_ricento/sync_job_listing'),
+            'entity_id_010'         => $resource->getTableName('sales/quote'),
+            'item_id_011'           => $resource->getTableName('sales/quote_item'),
+            'entity_id_012'         => $resource->getTableName('sales/order'),
+            'item_id_013'           => $resource->getTableName('sales/order_item'),
+            'entity_id_014'         => $resource->getTableName('sales/order_payment')
+        );
+
+        $files = array();
+        foreach ($tables as $key => $table) {
+
+            $cols = array('*');
+            if (in_array($key, array('entity_id_010', 'entity_id_010'))) {
+                $cols = $read->fetchCol('describe ' . $table);
+                $found = array_search('customer_email', $cols);
+                if ($found !== false) {
+                    unset($cols[$found]);
+                }
+                $found = array_search('customer_firstname', $cols);
+                if ($found !== false) {
+                    unset($cols[$found]);
+                }
+                $found = array_search('customer_lastname', $cols);
+                if ($found !== false) {
+                    unset($cols[$found]);
+                }
+            }
+
+            $select = $read
+                ->select()
+                ->from($table, $cols)
+                ->limit(500)
+                ->order(array( substr($key, 0, -4) . ' DESC'));
+
+
+
+            $object = new Varien_Object($read->fetchAll($select));
+            $files[] = $this->writeFile($destination, $table . '.csv', $object->toArray(), true);
+        }
+
+        return $files;
+    }
+
+    /**
+     * @param $destination
+     * @return mixed
+     */
+    public function getPhpExtensions($destination)
+    {
+        $content = implode("\n", get_loaded_extensions());
+        return $this->writeFile($destination, 'php_extensions.txt', $content);
+    }
+
+    /**
+     * @param $destination
+     * @return mixed
+     */
+    public function getMagentoConfig($destination)
+    {
+        $countSimpleProduct = Mage::getResourceModel('catalog/product_collection')
+            ->setStoreId(0)
+            ->addFieldToFilter('type_id', Mage_Catalog_Model_Product_Type::TYPE_SIMPLE)
+            ->getSize();
+
+        $countConfigurableProduct = Mage::getResourceModel('catalog/product_collection')
+            ->setStoreId(0)
+            ->addFieldToFilter('type_id', Mage_Catalog_Model_Product_Type::TYPE_CONFIGURABLE)
+            ->getSize();
+
+        $countGroupedProduct = Mage::getResourceModel('catalog/product_collection')
+            ->setStoreId(0)
+            ->addFieldToFilter('type_id', Mage_Catalog_Model_Product_Type::TYPE_GROUPED)
+            ->getSize();
+
+        $user = Mage::getSingleton('admin/session')->getUser();
+        $content = 'Current Backend User: ' . $user->getUsername() . ' - Email: ' . $user->getEmail() . "\n";
+        $content .= 'Magento Version: ' . Mage::getVersion() . ' ' . Mage::getEdition() . ' Edition' . "\n";
+        $content .= "Qty of: Simple Products #$countSimpleProduct - Configurable Products: #$countConfigurableProduct - Grouped Product: #$countGroupedProduct";
+
+        return $this->writeFile($destination, 'magento_info.txt', $content);
+    }
+
+    /**
+     * @param $destination
+     * @return mixed
+     */
+    public function getMagentoModules($destination)
+    {
+        $modules = (array) Mage::getConfig()->getModuleConfig();
+
+        $list = array();
+        foreach ($modules as $key => $module) {
+            $list[$key] = array('version' => $module->version, 'code_pool' => $module->codePool);
+        }
+
+        $content = new Varien_Object($list);
+
+        return $this->writeFile($destination, 'magento_modules.json', $content->toJson());
+    }
+
+    /**
+     * @param $destination
+     * @param $content
+     * @return mixed
+     * @throws Exception
+     */
+    public function writeFile($destination, $filename, $content, $csv = false)
+    {
+        $file = $destination . DS . $filename;
+        $ioFile = new Varien_Io_File();
+        $ioFile->cd($destination);
+        $ioFile->streamOpen($file);
+        if ($csv) {
+            foreach ($content as $row) {
+                $ioFile->streamWriteCsv($row);
+            }
+        } else {
+            $ioFile->streamWrite($content);
+        }
+        $ioFile->streamClose();
+
+        return $file;
+    }
+    
+    /**
      * @return array|mixed
      */
     public function getPhpInfoArray()
@@ -68,5 +276,63 @@ class Diglin_Ricento_Helper_Support extends Mage_Core_Helper_Abstract
         }
 
         return $pi;
+    }
+
+    public function sendConfigurationFile($content, $filename = 'ricardo_support.tar.gz', $replyTo = '', $message = '')
+    {
+        $template = 'ricento_support';
+
+        try {
+            if (! Mage::getConfig()->getNode(Mage_Core_Model_Email_Template::XML_PATH_TEMPLATE_EMAIL . '/' . $template)) {
+                Mage::throwException(Mage::helper('diglin_ricento')->__('Wrong transactional support email template.'));
+            }
+
+            $recipient = array(
+                'name' => 'Support Diglin',
+                'email' => $this->getSupportEmail()
+            );
+
+            $translate = Mage::getSingleton('core/translate');
+            $translate->setTranslateInline(false);
+
+            /* @var $emailTemplate Mage_Core_Model_Email_Template */
+            $emailTemplate = Mage::getModel('core/email_template')
+                ->setDesignConfig(array('area' => 'adminhtml', 'store' => 0));
+
+            if (!empty($replyTo)) {
+                $emailTemplate->setReplyTo($replyTo)
+                    ->setReturnPath($replyTo);
+            }
+
+            $variables = array(
+                'shopname' => Mage::getBaseUrl(),
+                'message' => $message
+            );
+
+            $attachment = $emailTemplate->getMail()->createAttachment($content);
+            $attachment->type = 'application/octet-stream';
+            $attachment->disposition = Zend_Mime::DISPOSITION_ATTACHMENT;
+            $attachment->encoding = Zend_Mime::ENCODING_BASE64;
+            $attachment->filename = $filename;
+
+            $emailTemplate->sendTransactional($template, // xml path email template
+                Mage::getStoreConfig('contacts/email/sender_email_identity'),//sender - normally general contact
+                $recipient['email'],
+                $recipient['name'],
+                $variables,
+                0);
+
+            $translate->setTranslateInline(true);
+
+        } catch (Exception $e) {
+            Mage::logException($e);
+        }
+
+        return;
+    }
+
+    public function getSupportEmail()
+    {
+        return str_replace('[/at/]', '@', Mage::getStoreConfig('support/email'));
     }
 }
--- src/app/code/community/Diglin/Ricento/controllers/Adminhtml/SupportController.php
+++ src/app/code/community/Diglin/Ricento/controllers/Adminhtml/SupportController.php
@@ -35,171 +35,24 @@ class Diglin_Ricento_Adminhtml_SupportController extends Mage_Adminhtml_Controll
 
     public function exportAction()
     {
-        $folder = Mage::getBaseDir('tmp') . DS . 'export' . DS . 'ricento';
-        $io = new Varien_Io_File();
-        $io->mkdir($folder);
-
-        $this->_getPhpInfo($folder);
-        $this->_getPhpExtensions($folder);
-        $this->_getRicentoConfig($folder);
-        $this->_getDeveloperConfig($folder);
-        $this->_getRicentoTables($folder);
-        $this->_getMagentoModules($folder);
-        $this->_getMagentoConfig($folder);
-
-        $exceptionFile = Mage::getStoreConfig(Mage_Core_Model_Session_Abstract::XML_PATH_LOG_EXCEPTION_FILE);
-//        $systemFile = Mage::getStoreConfig('dev/log/file');
-
-        $io->cp(Mage::getBaseDir('var') . DS . 'log' . DS . Diglin_Ricento_Helper_Data::LOG_FILE, $folder . DS . Diglin_Ricento_Helper_Data::LOG_FILE);
-        $io->cp(Mage::getBaseDir('var') . DS . 'log' . DS . $exceptionFile, $folder . DS . $exceptionFile);
-
-        $destination = $folder . 'tar';
-        $tar = new Mage_Archive_Tar();
-        $tar->pack($folder, $destination);
-
-        $gzDestination = $destination . '.gz';
-        $tar = new Mage_Archive_Gz();
-        $tar->pack($destination, $gzDestination);
-
-        $io->rmdir($folder, true);
-
+        $gzDestination = Mage::helper('diglin_ricento/support')->exportAll();
         return $this->_prepareDownloadResponse('ricardo_support.tar.gz', file_get_contents($gzDestination));
     }
 
-    /**
-     * @param $destination
-     * @return mixed
-     */
-    protected function _getPhpInfo($destination)
+    public function sendAction()
     {
-        $phpinfo = new Varien_Object(Mage::helper('diglin_ricento/support')->getPhpInfoArray());
-        return $this->_writeFile($destination, 'phpinfo.json', $phpinfo->toJson());
-    }
+        $helper = Mage::helper('diglin_ricento/support');
 
-    /**
-     * @param $destination
-     * @return mixed
-     */
-    protected function _getRicentoConfig($destination)
-    {
-        $xml = Mage::getConfig()->getNode('default/ricento')->asNiceXml();
-        return $this->_writeFile($destination, 'ricento_config.xml', $xml);
-    }
-
-    /**
-     * @param $destination
-     * @return mixed
-     */
-    protected function _getDeveloperConfig($destination)
-    {
-        $xml = Mage::getConfig()->getNode('default/dev')->asNiceXml();
-        return $this->_writeFile($destination, 'config_dev.xml', $xml);
-    }
-
-    /**
-     * @param $destination
-     * @return array
-     */
-    protected function _getRicentoTables($destination)
-    {
-        $resource = Mage::getSingleton('core/resource');
-        $read = $resource->getConnection('core_read');
-
-        $tables = array(
-            'entity_id_001'         => $resource->getTableName('diglin_ricento/api_token'),
-            'entity_id_002'         => $resource->getTableName('diglin_ricento/products_listing'),
-            'item_id_003'           => $resource->getTableName('diglin_ricento/products_listing_item'),
-            'log_id_004'            => $resource->getTableName('diglin_ricento/listing_log'),
-            'entity_id_005'         => $resource->getTableName('diglin_ricento/sales_options'),
-            'transaction_id_006'    => $resource->getTableName('diglin_ricento/sales_transaction'),
-            'rule_id_007'           => $resource->getTableName('diglin_ricento/shipping_payment_rule'),
-            'job_id_008'            => $resource->getTableName('diglin_ricento/sync_job'),
-            'job_listing_id_009'    => $resource->getTableName('diglin_ricento/sync_job_listing'),
-            'entity_id_010'         => $resource->getTableName('sales/quote'),
-            'item_id_011'           => $resource->getTableName('sales/quote_item'),
-            'entity_id_012'         => $resource->getTableName('sales/order'),
-            'item_id_013'           => $resource->getTableName('sales/order_item'),
-            'entity_id_014'         => $resource->getTableName('sales/order_payment')
-        );
-
-        $files = array();
-        foreach ($tables as $key => $table) {
-
-            $select = $read
-                ->select()
-                ->from($table)
-                ->limit(500)
-                ->order(array( substr($key, 0, -4) . ' DESC'));
-
-            $object = new Varien_Object($read->fetchAll($select));
-            $files[] = $this->_writeFile($destination, $table . '.csv', $object->toArray(), true);
+        try {
+            $gzDestination = $helper->exportAll();
+            $helper->sendConfigurationFile(file_get_contents($gzDestination));
+            $this->_getSession()->addSuccess($this->__('Configuration successfully sent. If not already done, please <a href="mailto:%s?subject=ricardo.ch Magento Extension Support">contact us</a> to explain us your issue.', $helper->getSupportEmail()));
+        } catch (Exception $e) {
+            Mage::logException($e);
+            $this->_getSession()->addError('A problem occured while trying to send your configuration per email. Please, review your log file');
         }
 
-        return $files;
-    }
+        $this->_redirect('adminhtml/system_config/edit/section/ricento');
 
-    /**
-     * @param $destination
-     * @return mixed
-     */
-    protected function _getPhpExtensions($destination)
-    {
-        $content = implode("\n", get_loaded_extensions());
-        return $this->_writeFile($destination, 'php_extensions.txt', $content);
-    }
-
-    /**
-     * @param $destination
-     * @return mixed
-     */
-    protected function _getMagentoConfig($destination)
-    {
-        $adminSession = Mage::getSingleton('admin/session');
-        $content = 'Current Backend User: ' . $adminSession->getUser()->getUsername() . ' - Email: ' . $adminSession->getUser()->getEmail() . "\n";
-        $content .= 'Magento Version: ' . Mage::getVersion() . ' ' . Mage::getEdition() . ' Edition' . "\n";
-
-        return $this->_writeFile($destination, 'magento_info.txt', $content);
-    }
-
-    /**
-     * @param $destination
-     * @return mixed
-     */
-    protected function _getMagentoModules($destination)
-    {
-        $modules = (array) Mage::getConfig()->getModuleConfig();
-
-        $list = array();
-        foreach ($modules as $key => $module) {
-            $list[$key] = array('version' => $module->version, 'code_pool' => $module->codePool);
-        }
-
-        $content = new Varien_Object($list);
-
-        return $this->_writeFile($destination, 'magento_modules.json', $content->toJson());
-    }
-
-    /**
-     * @param $destination
-     * @param $content
-     * @return mixed
-     * @throws Exception
-     */
-    protected function _writeFile($destination, $filename, $content, $csv = false)
-    {
-        $file = $destination . DS . $filename;
-        $ioFile = new Varien_Io_File();
-        $ioFile->cd($destination);
-        $ioFile->streamOpen($file);
-        if ($csv) {
-            foreach ($content as $row) {
-                $ioFile->streamWriteCsv($row);
-            }
-        } else {
-            $ioFile->streamWrite($content);
-        }
-        $ioFile->streamClose();
-
-        return $file;
     }
 }
